3047a37746028c538863c2e16e784ec4127a7853
BrunoFiorine <brunocmfiorine@gmail.com>
2025-08-24 13:31:09 -0300

    adição da nova calculadora

---
+++ calculadora2.0.js
@@ -0,0 +1,15 @@
+// Calculadora 2.0 // Operações matemáticas básicas
+
+let num1 = 25;
+let num2 = 5;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicação = num1 * num2;
+let divisão = num1/num2;
+
+
+console.log("A valor da soma é: ", soma);
+console.log("A valor dasubtração é: ", subtracao);
+console.log("A valor damultiplicação é: ", multiplicação);
+console.log("A valor dadivisão é: ", divisão);
